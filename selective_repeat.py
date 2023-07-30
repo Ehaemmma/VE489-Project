68f36b19ad4021c0a4835c1cf22ec710da0c65ce
SU Zhenxuan <sinesu@sjtu.edu.cn>
2023-07-30 19:36:57 +0800

    testing parameter: window size fixed.

--- selective_repeat.py
+++ selective_repeat.py
@@ -178,7 +178,7 @@ if __name__ == "__main__":
     ack_size = 25 * 8  # bits
     header_size = 25 * 8  # bit
     num_frames = 100000
-    window_size = 500
+    window_size = 20
     frame_error_rate = 1 - (1 - bit_error_rate) ** (frame_size)
     print(frame_error_rate)
 
